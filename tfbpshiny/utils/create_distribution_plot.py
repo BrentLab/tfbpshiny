843a291e4b3ecc4b7dad1e6388f1de5f296cde8a
chasem <chasem@wustl.edu>
2025-05-20 18:08:41 -0500

    adding logging and input type checking to create_distribution_plot

--- tfbpshiny/utils/create_distribution_plot.py
+++ tfbpshiny/utils/create_distribution_plot.py
@@ -1,9 +1,13 @@
+import logging
+
 import pandas as pd
 import plotly.express as px
 from plotly.graph_objects import Figure
 
 from .plot_formatter import apply_plot_formatter
 from .rename_dataframe_data_sources import rename_dataframe_data_sources
+
+logger = logging.getLogger("shiny")
 
 
 def create_distribution_plot(
@@ -20,8 +24,13 @@ def create_distribution_plot(
     :param y_axis_title: Title for the y-axis
     :param category_orders: Optional dict of category orders
     :return: A formatted plotly figure
+    :raises TypeError: If df is not a pandas DataFrame
 
     """
+    if not isinstance(df, pd.DataFrame):
+        logger.error("Input df is not a pandas DataFrame")
+        raise TypeError("Input df must be a pandas DataFrame")
+
     df_renamed = rename_dataframe_data_sources(df)
 
     # Default category orders if not provided
