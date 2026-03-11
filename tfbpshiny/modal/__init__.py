ebdf1f7a4259540b046533148fbbd1319559cb10
chasem <chasem@wustl.edu>
2026-03-11 18:53:27 -0500

    remove modal subdir

--- tfbpshiny/modal/__init__.py
+++
@@ -1,9 +0,0 @@
-from tfbpshiny.modal.intersection import (
-    render_intersection_detail_modal,
-    resolve_analysis_module,
-)
-
-__all__ = [
-    "render_intersection_detail_modal",
-    "resolve_analysis_module",
-]
